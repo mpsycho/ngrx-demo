efe34e9e30ed2a01907574fb59fc1b8c836f968f
psycho715 <marios.psychogios@gmail.com>
2025-08-26 11:55:14 +0300

    commit 3 - create actions

--- src/app/products/products-page/products-page.component.ts
+++ src/app/products/products-page/products-page.component.ts
@@ -3,6 +3,7 @@ import { sumProducts } from 'src/app/utils/sum-products';
 import { Product } from '../product.model';
 import { ProductsService } from '../products.service';
 import { Store } from '@ngrx/store';
+import { ProductsAPIActions, ProductsPageActions } from '../state/products.actions';
 
 @Component({
   selector: 'app-products-page',
@@ -10,9 +11,9 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./products-page.component.css'],
 })
 export class ProductsPageComponent {
-  products: Product[] = [];
+  products$ = this.store.select((state: any) => state.products.products);
   total = 0;
-  loading = true;
+  loading$ = this.store.select((state: any) => state.products.loading);
   showProductCode$ = this.store.select(
     (state: any) => state.products.showProductCode
   );
@@ -27,17 +28,19 @@ export class ProductsPageComponent {
   }
 
   getProducts() {
+    this.store.dispatch(ProductsPageActions.loadProducts());
     this.productsService.getAll().subscribe({
       next: (products) => {
-        this.products = products;
+        this.store.dispatch(
+          ProductsAPIActions.productsLoadedSuccess({ products })
+        );
         this.total = sumProducts(products);
-        this.loading = false;
       },
       error: (error) => (this.errorMessage = error),
     });
   }
 
   toggleShowProductCode() {
-    this.store.dispatch({ type: '[Products Page] Toggle Show Product Code' });
+    this.store.dispatch(ProductsPageActions.toggleShowProductCode());
   }
 }
